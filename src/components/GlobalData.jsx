136e245ca3dbfc0233c21d8ed7a5201da8242cd2
syed-rauf <masterrufi.it@gmail.com>
2022-03-26 19:28:53 +0500

    countup added

--- src/components/GlobalData.jsx
+++ src/components/GlobalData.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import CountUp from "react-countup";
 import { Paper, Box, Typography } from "@mui/material";
 //components
 import { globalContext } from "../GlobalContext/GlobalContext";
@@ -29,8 +30,16 @@ export default function GlobalData() {
       >
         <Paper elevation={2} sx={{ bgcolor: "#274472", color: "#C3E0E5" }}>
           <Typography variant="h6">Active Cases</Typography>
-          <Typography variant="h5" color="blue">
-            {selectedCountry ? selectedCountry[0].cases.active : "Loading"}
+          <Typography variant="h5" color="rgba(55, 105, 240, 1)">
+            {selectedCountry ? "" : "Loading"}
+            {selectedCountry && (
+              <CountUp
+                start={0}
+                end={selectedCountry[0].cases.active}
+                duration={2.5}
+                separator=","
+              />
+            )}
           </Typography>
           <Typography variant="Subtitle2">{currentDate}</Typography>
         </Paper>
@@ -38,7 +47,15 @@ export default function GlobalData() {
         <Paper elevation={2} sx={{ bgcolor: "#274472", color: "#C3E0E5" }}>
           <Typography variant="h6">Total Recoverd</Typography>
           <Typography variant="h5" color="orange">
-            {selectedCountry ? selectedCountry[0].cases.recovered : "Loading"}
+            {selectedCountry ? "" : "Loading"}
+            {selectedCountry && (
+              <CountUp
+                start={0}
+                end={selectedCountry[0].cases.recovered}
+                duration={2.5}
+                separator=","
+              />
+            )}
           </Typography>
           <Typography variant="Subtitle2">{currentDate}</Typography>
         </Paper>
@@ -46,7 +63,15 @@ export default function GlobalData() {
         <Paper elevation={2} sx={{ bgcolor: "#274472", color: "#C3E0E5" }}>
           <Typography variant="h6">Total Deaths</Typography>
           <Typography variant="h5" color="error">
-            {selectedCountry ? selectedCountry[0].deaths.total : "Loading"}
+            {selectedCountry ? "" : "Loading"}
+            {selectedCountry && (
+              <CountUp
+                start={0}
+                end={selectedCountry[0].deaths.total}
+                duration={2.5}
+                separator=","
+              />
+            )}
           </Typography>
           <Typography variant="Subtitle2">{currentDate}</Typography>
         </Paper>
@@ -54,7 +79,15 @@ export default function GlobalData() {
         <Paper elevation={2} sx={{ bgcolor: "#274472", color: "#C3E0E5" }}>
           <Typography variant="h6">Total Cases</Typography>
           <Typography variant="h5" color="rgba(75, 192, 92, 1)">
-            {selectedCountry ? selectedCountry[0].cases.total : "Loading"}
+            {selectedCountry ? "" : "Loading"}
+            {selectedCountry && (
+              <CountUp
+                start={0}
+                end={selectedCountry[0].cases.total}
+                duration={2.5}
+                separator=","
+              />
+            )}
           </Typography>
           <Typography variant="Subtitle2">{currentDate}</Typography>
         </Paper>
